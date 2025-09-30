286a381c595b721919bc857498fd1636516186e1
sofiaamandi <amandi1691@gmail.com>
2025-09-30 14:31:00 +0200

    Create theme.js

---
+++ assets/theme.js
@@ -0,0 +1,9 @@
+<script>
+  // Paleta marina común
+  tailwind = { config: {
+    theme: { extend: { colors: { marine: {
+      50:'#F4FAFD',100:'#E6F3FB',200:'#CFE8F7',300:'#A9D4EF',400:'#7EBBE4',
+      500:'#4D9FD6',600:'#2D7FB7',700:'#1E5E8A',800:'#164565',900:'#0F2E45'
+    }}}}
+  }};
+</script>
